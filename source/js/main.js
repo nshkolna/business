1fbfc1fe80a8bf9a2754c73a971146191d6f2773
Natali Shkolna <nshkolna@gmail.com>
2019-03-14 16:34:44 +0200

    Add adaptive layout

---
+++ source/js/main.js
@@ -0,0 +1,39 @@
+(function() {
+	// Add event listener for open form button
+	var openFormButton = document.querySelector('.arrow-down');
+	var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
+
+	if(openFormButton){
+		
+		openFormButton.addEventListener('click', function(e){
+			e.preventDefault();
+			BUSINESS.form.open();
+		})
+	}
+
+	if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (BUSINESS.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+
+        })
+    }
+
+    if (nav) {
+        nav.addEventListener('click', function(e) {
+            var target = e.target;
+
+            if (target.tagName.toLowerCase() !== 'a') {
+                return;
+            }
+
+            e.preventDefault();
+            BUSINESS.navigation.toggleToActiveLink(target);
+        });
+    }
+}());
